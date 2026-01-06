01332ead3143780202341443c936ad140b94801d
Gobbledyglomp <skoobidyboo@pm.me>
2026-01-06 14:20:26 +0200

    Fix the bug when react router endpoints break on reload

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const express = require('express')
+const path = require('path')
 
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
@@ -45,6 +46,12 @@ app.use('/api/login', loginRouter)
 if (config.ENV === 'test') {
   app.use('/api/testing', testingRouter)
 }
+app.use((req, res, next) => {
+  if (req.method === 'GET' && !req.path.startsWith('/api')) {
+    return res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+  }
+  next()
+})
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
